2628f83f27cee4dbac94d7a3578854fdb405e847
daniel.gavino <daniel.gavino@acisa.es>
2017-12-13 11:32:00 +0100

    Añadido huelva y cadiz
    añadido huelva y cadiz

--- GoogleMap/map.py
+++ GoogleMap/map.py
@@ -9,8 +9,8 @@ from datetime import datetime
 #pip install requests
 
 
-def get_tiempo_recorrido(work):
-    home_coord = "Mairena+del+Aljarafe,Sevilla"
+def get_tiempo_recorrido(home, work):
+    home_coord = str(home.strip())
     work_coord = str(work.strip())
     departure_time = "now"
     print "work_coord:%s" % work_coord
@@ -28,13 +28,15 @@ def get_tiempo_recorrido(work):
     return str(driving_time_min)
 
 
-def main():
+def get_sevilla():
 
     csvfile = open("loc_sevilla.csv", 'rb')
     entrada = csv.reader(csvfile, delimiter=';')
 
-    target = open("Results.csv", 'a')
+    target = open("ResultsSevilla.csv", 'a')
     salida = csv.writer(target, delimiter=';')
+
+    home_coord = "Mairena+del+Aljarafe,Sevilla"
 
     for reg in entrada:
         codigo = str(reg[0])
@@ -54,13 +56,55 @@ def main():
         direccion = ciudad_final + "," + provincia_final
         print "direccion:%s" % direccion
 
-        tiempo = get_tiempo_recorrido(direccion)
+        tiempo = get_tiempo_recorrido(home_coord, direccion)
         print "tiempo:%s" % tiempo
 
         salida.writerow([codigo, ciudad_2, provincia, tiempo])
 
     csvfile.close()
     target.close()
+
+
+def get_huelva():
+
+    csvfile = open("loc_huelva.csv", 'rb')
+    entrada = csv.reader(csvfile, delimiter=';')
+
+    target = open("ResultsHuelva.csv", 'a')
+    salida = csv.writer(target, delimiter=';')
+
+    home_coord = "Mairena+del+Aljarafe,Sevilla"
+
+    for reg in entrada:
+        codigo = str(reg[0])
+        codigo = codigo.strip()
+        print "codigo:%s" % codigo
+
+        ciudad = str(reg[1])
+        ciudad_2 = ciudad.strip()
+        ciudad_sin_tildes = ciudad_2
+        ciudad_final = ciudad_sin_tildes.replace(' ', '+')
+        print "ciudad:%s" % ciudad_final
+
+        provincia = str(reg[2])
+        provincia_final = provincia.strip()
+        print "provincia:%s" % provincia_final
+
+        direccion = ciudad_final + "," + provincia_final
+        print "direccion:%s" % direccion
+
+        tiempo = get_tiempo_recorrido(home_coord, direccion)
+        print "tiempo:%s" % tiempo
+
+        salida.writerow([codigo, ciudad_2, provincia, tiempo])
+
+    csvfile.close()
+    target.close()
+
+
+def main():
+
+    get_huelva()
 
 
 if __name__ == '__main__':
